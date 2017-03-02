8c1a392960c5ff0a3e4f1c734074205b6ba5480f
Erfan Sami <erfansami007@gmail.com>
2017-03-02 23:39:26 +0600

    Add files via upload

---
+++ Mid Exam Solution 1 .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std ;
+
+int main()
+{
+
+    string hey ; //string variable
+    cout<<  " \nWhat is your name? Please enter your name:\n "  ; //display and print
+    cin>> hey ;  //user input
+    cout << "Hello there \n" <<hey ;  //output and called back hey variable
+
+
+
+    return 0 ;
+}
