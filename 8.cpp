28b78eec20a3f38764dc5cf4d589b8a91422fccb
Erfan Sami <erfansami007@gmail.com>
2017-03-09 09:49:07 -0800

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>   //This includes input, output library
+#include <math.h>    
+using namespace std;
+ 
+int main() //This is the main function
+{        //Start of main body
+    
+    float a,b;      // Declares float variable with label
+    
+    cout<<"Input the radius of the sphere : ";  // display to the user
+    
+    cin>>a;                  // takes input from user
+    
+    b=(4/3)*3.142*(r*r*r);    // formula in b variable
+    
+    cout<<"The volume of sphere is : "<<b;   // display and ans of b variable formula
+   
+    return 0; //End of program
+   
+}     //End of main body
